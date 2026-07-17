d3aceb38701bc2bd751d915d5659f8474480cd0f
8848 Momo House <8848momohouses@gmail.com>
2026-07-17 18:12:16 +1000

    Fix Odoo schema update port conflicts

--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -13,10 +13,16 @@ docker compose up -d --remove-orphans
 echo "Applying Odoo internal security hotfixes..."
 sleep 5
 docker exec -u root 8848_odoo sed -i 's/if not regex_pg_name.match(name):/if False:/g' /usr/lib/python3/dist-packages/odoo/orm/utils.py || true
+echo "Stopping Odoo to perform safe database upgrade..."
+docker compose stop odoo
+
 echo "Updating Odoo database schema to match new code..."
 MODULES="8848_api_gateway,8848_communication,8848_core_branding,8848_crm,8848_dashboard,8848_delivery,8848_driver,8848_factory,8848_franchise,8848_glass_skin,8848_inventory,8848_marketing_fee,8848_portal,8848_quality,8848_royalty,8848_security,8848_store_performance,8848_supplier,8848_warehouse,8848_workflow"
-docker exec -u odoo 8848_odoo odoo -c /etc/odoo/odoo.conf -d 8848_live_final -i $MODULES --stop-after-init --http-port=8079
-docker restart 8848_odoo
+# We output logs to stdout so we can see them in GitHub Actions!
+docker compose run --rm odoo odoo -c /etc/odoo/odoo.conf -d 8848_live_final -i $MODULES --stop-after-init --logfile=/dev/stdout
+
+echo "Starting Odoo back up..."
+docker compose up -d --remove-orphans
 
 echo "Deployment triggered successfully."
 echo "Running healthcheck..."
